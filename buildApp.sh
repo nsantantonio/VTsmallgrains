bb6944f390c1fc2a3465b136fa770bae92091a90
Nicholas Santantonio <ns722@cornell.edu>
2020-02-10 17:59:43 -0500

    added build

---
+++ buildApp.sh
@@ -0,0 +1,14 @@
+pdflatex SantantonioCoverLetter.tex
+pdflatex SantantonioResearchStatement.tex 
+pdflatex SantantonioTeachingStatement.tex 
+pdflatex SantantonioContributionToDiversityEquityInclusion.tex
+
+pdflatex SantantonioCoverLetter.tex
+pdflatex SantantonioResearchStatement.tex 
+pdflatex SantantonioTeachingStatement.tex 
+pdflatex SantantonioContributionToDiversityEquityInclusion.tex
+
+cp SantantonioCoverLetter.tex SantantonioCoverLetterNoSig.tex
+sed -i '/includegraphics/c\\\vspace{2cm}' SantantonioCoverLetterNoSig.tex
+pdflatex SantantonioCoverLetterNoSig.tex
+pdflatex SantantonioCoverLetterNoSig.tex
